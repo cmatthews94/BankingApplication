6174a064a567d262c1cfdea1378e84afec75c087
cmatthews94 <claire.matthews.94@outlook.com>
2024-06-17 16:33:35 +0100

    blazor project initial commit
    added and started work on Blazor project for the webpage portion of the proposal

---
+++ BankingApplicationBlazorWebpage/Program.cs
@@ -0,0 +1,40 @@
+using BankingApplication.Interfaces;
+using BankingApplication.LogicClasses;
+using BankingApplication;
+using BankingApplicationBlazorWebpage.Components;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddMudServices();
+
+builder.Services.AddDbContext<BankingApplicationDbContext>(options =>
+    options.UseSqlServer(@"Data Source=.\HYDAELYN;Initial Catalog=BankingApplicationDb;Integrated Security=True;Encrypt=False;"));
+builder.Services.AddTransient<IBankUserService, BankUserService>();
+builder.Services.AddTransient<IBankAccountService, BankAccountService>();
+builder.Services.AddTransient<ITransactionService, TransactionService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+
